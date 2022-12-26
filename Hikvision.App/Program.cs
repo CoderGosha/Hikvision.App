bf2a6451bf63d99f0d942e70d0f9a9803cb274f3
Igor Pakholkov <coder.gosha@gmail.com>
2022-12-26 13:09:46 +0300

    Добавлен алгоритм ожидания

--- Hikvision.App/Program.cs
+++ Hikvision.App/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Hikvision.Api;
+using Hikvision.Api.ResponseModels;
 
 namespace HikvisionApp
 {
     class Program
     {
-        public static void SavePhotoFileWithBinaryWriter(byte[] data, string prefixPath, string number)
+        public static string SavePhotoFileWithBinaryWriter(byte[] data, string prefixPath, string number)
         {
             var dir = Directory.GetCurrentDirectory();
             var basePath = Path.Join(dir, prefixPath);
@@ -16,11 +18,30 @@ namespace HikvisionApp
 
             var fileName = $"{number}_" + DateTime.Now.ToString("yyyy_MM_dd_H-mm-ss") + ".jpg";
             var filePath = Path.Join(basePath, fileName);
-            
+
             using var writer = new BinaryWriter(File.OpenWrite(filePath));
             writer.Write(data);
+            return filePath;
         }
-        
+
+        public static async Task ManualCap(IHikvisionApi hikvison)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var manualCup = await hikvison.ManualCupAsync();
+            if (!manualCup.IsSuccess)
+            {
+                Console.WriteLine($"Error manual cup: {manualCup.Code} message: {manualCup.Message}");
+                return;
+            }
+
+            // Метод сохранения фото
+            var filePath = SavePhotoFileWithBinaryWriter(manualCup.Data.Image, "manual_cup", manualCup.Data.Number);
+            sw.Stop();
+            Console.WriteLine(
+                $"Manual cup-{sw.ElapsedMilliseconds}ms: Recognize: {manualCup.Data?.IsRecognize} Number: {manualCup.Data?.Number} Image: {filePath}");
+        }
+
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -49,16 +70,52 @@ namespace HikvisionApp
                 $"Device: {deviceInfo?.Data?.DeviceName}, ID: {deviceInfo?.Data?.DeviceID}, " +
                 $"FWVersion: {deviceInfo?.Data?.FirmwareVersion}, FWData: {deviceInfo?.Data?.FirmwareReleasedDate}");
 
-            var manualCup = await hikvison.ManualCupAsync();
-            if (!manualCup.IsSuccess)
+            await ManualCap(hikvison);
+
+            string exitKey = "x";
+            string callSign;
+            // 
+            Console.WriteLine("Press key to manual cup, W to manual cup with timeout or X to exit: ");
+            while ((callSign = Console.ReadLine().ToLower()) != exitKey)
             {
-                Console.WriteLine($"Error manual cup: {manualCup.Code} message: {manualCup.Message}");
-                return;
+                if (callSign == "w")
+                {
+                    await ManualWaitCap(hikvison, 10);
+                }
+                else
+                    await ManualCap(hikvison);
+
+                Console.WriteLine("");
+                Console.WriteLine("Enter a call sign to find in the list. Press X to exit: "); //Prompt
             }
-            
-            Console.WriteLine($"Manual cup: Recognize: {manualCup.Data?.IsRecognize} Number: {manualCup.Data?.Number}");
-            // Метод сохранения фото
-            SavePhotoFileWithBinaryWriter(manualCup.Data.Image, "manual_cup", manualCup.Data.Number);
+        }
+
+        private static async Task ManualWaitCap(IHikvisionApi hikvison, int timeout)
+        {
+            Console.WriteLine($"{DateTime.Now} - Starting manual wait");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            BaseResponse<ManualCupResponse> manualCup = null;
+            var timeStart = DateTime.Now + TimeSpan.FromSeconds(timeout);
+            while ((timeStart > DateTime.Now) || (manualCup == null))
+            {
+                manualCup = await hikvison.ManualCupAsync();
+                if (manualCup.Data.IsRecognize)
+                {
+                    var filePath =
+                        SavePhotoFileWithBinaryWriter(manualCup.Data.Image, "manual_cup", manualCup.Data.Number);
+                    sw.Stop();
+                    Console.WriteLine(
+                        $"{DateTime.Now} - Manual cup-{sw.ElapsedMilliseconds}ms: Recognize: {manualCup.Data?.IsRecognize} Number: {manualCup.Data?.Number} Image: {filePath}");
+                }
+            }
+
+            Console.WriteLine($"{DateTime.Now} - Timeout manual wait");
+            var filePathUnknown =
+                SavePhotoFileWithBinaryWriter(manualCup.Data.Image, "manual_cup", manualCup.Data.Number);
+            sw.Stop();
+            Console.WriteLine(
+                $"{DateTime.Now} - Manual cup-{sw.ElapsedMilliseconds}ms: Recognize: {manualCup.Data?.IsRecognize} Number: {manualCup.Data?.Number} Image: {filePathUnknown}");
         }
     }
 }
